5671e95d5e7a9e40beb3a98ed43c12d2bb96fd62
Jan Chorowski <jan.chorowsk@gmail.com>
2014-10-16 10:42:05 -0400

    Hierarchic configuration demo

--- hierarchiconf/tests/hierarchiconf_test.py
+++ hierarchiconf/tests/hierarchiconf_test.py
@@ -6,18 +6,52 @@ Created on Oct 8, 2014
 import unittest
 import hierarchiconf
 
-Conf = hierarchiconf.Conf
 
-sample_conf = Conf({'//': Conf({'seed':[1,2,3],
-                                }),
-                    'block1/seed': [4,5,6], 
-                    'block2' : Conf({
-                        'seed':'b2seed',
-                        'dvar':dict(imadict=True),             
-                        })                                
-                    })
 
 class Test(unittest.TestCase):
+    def test_operational(self):
+        Conf = hierarchiconf.Conf
+        root_conf = Conf({'//'        : Conf({
+                                              'seed':'global_seed',
+                                              }),
+                          'block1/seed': 'b1_seed',
+                          'block2'     : Conf({
+                                               'seed':'b2_seed',
+                                               'dict_var': dict(imadict=True),
+                                               })                                
+                          })
+        
+        self.assertEqual(root_conf['seed'], 'global_seed')
+        self.assertEqual(root_conf['block3/seed'], 'global_seed')
+        self.assertEqual(root_conf['block1/seed'], 'b1_seed')
+        b2_conf = root_conf.subconf('block2')
+        self.assertEqual(b2_conf['seed'], 'b2_seed')
+        self.assert_(isinstance(b2_conf['dict_var'], dict))
+        b2_conf['var'] ='val'
+        self.assertEqual(b2_conf['var'], 'val')
+        self.assertEqual(root_conf['block2/var'], 'val')
+        self.assertRaises(KeyError, lambda: root_conf['var'])
+        self.assertRaises(KeyError, lambda: root_conf['block1/var'])
+    
+    def test_special(self):
+        Conf = hierarchiconf.Conf
+        root_conf = Conf({'//'        : Conf({
+                                              'seed':'global_seed',
+                                              }),
+                          'block1/seed': 'b1_seed',
+                          'block2'     : Conf({
+                                               'seed':'b2_seed',
+                                               'dict_var': dict(imadict=True),
+                                               })                                
+                          })
+        self.assertEquals(root_conf['location'], '')
+        self.assertEquals(root_conf.subconf('block1')['name'], 'block1')
+        self.assertEquals(root_conf.subconf('block1')['location'], 'block1')
+        self.assertEquals(root_conf.subconf('block1/sb1')['name'], 'sb1')
+        self.assertEquals(root_conf.subconf('block1', 'sb2')['location'], 'block1/sb2')
+        self.assertEquals(root_conf['block1/sb2/location'], 'block1/sb2')
+        self.assertRaises(Exception, lambda : root_conf.__setitem__('block1/sb2/location','ttdd'))
+    
     def test_split_to_parts(self):
         sp = hierarchiconf._split_to_parts_valid
         self.assertEqual(sp(['a','b','c']), ['a','b','c'])
@@ -36,7 +70,7 @@ class Test(unittest.TestCase):
     def test_match(self):
         m = lambda s,p: hierarchiconf._match(hierarchiconf._split_to_parts_valid(s),
                                              hierarchiconf._split_to_parts_valid(p),
-                                             allow_prefix_match=False, 
+                                             do_prefix_match=False, 
                                              specificity=hierarchiconf._base_specificity)
         self.assertTrue(m(['lrate'], ['lrate']))
         self.assertTrue(m(['.*/lrate'], ['aa/lrate']))
