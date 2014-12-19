0a7f61bfc139404d434a4727a612e4e97341a1d7
Jan Chorowski <jan.chorowsk@gmail.com>
2014-12-19 19:24:33 +0100

    New selector interface

--- tests/test_select.py
+++ tests/test_select.py
@@ -1,26 +1,21 @@
-from copy import deepcopy
-
+from blocks.select import Selector, ChildSelector, AllDescendantsSelector,\
+    FieldSelector, AttributeMatchSelector, SelectorParseException
+from nose.tools import assert_raises
+from blocks.bricks import Brick
 import theano
 
-from blocks.bricks import Brick
-from blocks.select import Path, Selector
 
+def test_selector_parsing():
+    sel = Selector('/name1//name2.params@name==W')
+    s = sel.selectors
+    assert isinstance(s[0], ChildSelector)
+    assert isinstance(s[1], AttributeMatchSelector)
+    assert isinstance(s[2], AllDescendantsSelector)
+    assert isinstance(s[3], AttributeMatchSelector)
+    assert isinstance(s[4], FieldSelector)
+    assert isinstance(s[5], AttributeMatchSelector)
 
-def test_path():
-    path1 = Path.parse("/brick")
-    assert path1.nodes == (Path.BrickName("brick"),)
-
-    path2 = Path.parse("/brick.W")
-    assert path2.nodes == (Path.BrickName("brick"), Path.ParamName("W"))
-
-    path3 = Path.parse("/brick1/brick2")
-    assert path3.nodes == (Path.BrickName("brick1"), Path.BrickName("brick2"))
-
-    path4 = deepcopy(path3)
-    assert path4 == path3
-    assert path4 != path2
-    assert hash(path4) == hash(path3)
-    assert hash(path4) != hash(path2)
+    assert_raises(SelectorParseException, lambda: Selector('/name1//name2.params@name==W:'))
 
 
 def test_selector():
@@ -43,27 +38,49 @@ def test_selector():
     t1 = MockBrickTop([b1, b2], name="t1")
     t2 = MockBrickTop([b2, b3], name="t2")
 
-    s1 = Selector([t1])
-    s11 = s1.select("/t1/b1")
-    assert s11.bricks[0] == b1
-    assert len(s11.bricks) == 1
-    s12 = s1.select("/t1")
-    assert s12.bricks[0] == t1
-    assert len(s12.bricks) == 1
+    s1 = Selector("t1/b1").enumerate([t1])
+    assert b1 in s1
+    assert len(s1) == 1
 
-    s2 = Selector([t1, t2])
-    s21 = s2.select("/t2/b2")
-    assert s21.bricks[0] == b2
-    assert len(s21.bricks) == 1
+    s1 = Selector("t1/b1").enumerate([t1, t2])
+    assert b1 in s1
+    assert len(s1) == 1
 
-    assert s2.select("/t2/b2.V")[0] == b2.params[0]
+    s1 = Selector("t1/[.1]").enumerate([t1, t2])
+    assert b1 in s1
+    assert len(s1) == 1
 
-    params = list(s1.get_params().items())
-    assert params[0][0] == "/t1/b1.V"
-    assert params[0][1] == b1.params[0]
-    assert params[1][0] == "/t1/b1.W"
-    assert params[1][1] == b1.params[1]
-    assert params[2][0] == "/t1/b2.V"
-    assert params[2][1] == b2.params[0]
-    assert params[3][0] == "/t1/b2.W"
-    assert params[3][1] == b2.params[1]
+    s2 = Selector("t1").enumerate([t1])
+    assert t1 in s2
+    assert len(s2) == 1
+
+    s2 = Selector("t1").enumerate([t1, t2])
+    assert t1 in s2
+    assert len(s2) == 1
+
+    sp = Selector("t2/b2.params@name==V").enumerate([t1, t2])
+    assert b2.params[0] in sp
+
+    params = Selector('t1/b1.params').enumerate_paths([t1, t2])
+    assert params['t1/b1.params[V]'] == b1.params[0]
+    assert params['t1/b1.params[W]'] == b1.params[1]
+
+    params = Selector('//.params').enumerate([t1, t2])
+    assert b1.params[0] in params
+    assert b1.params[1] in params
+    assert b2.params[0] in params
+    assert b2.params[1] in params
+    assert b3.params[0] in params
+    assert b3.params[1] in params
+    assert len(params) == 6
+
+    params = Selector('//.params').enumerate_paths([t1, t2])
+    assert params['t1/b1.params[V]'] == b1.params[0]
+    assert params['t1/b1.params[W]'] == b1.params[1]
+    assert params['t1/b2.params[V]'] == b2.params[0]
+    assert params['t1/b2.params[W]'] == b2.params[1]
+    assert params['t2/b2.params[V]'] == b2.params[0]
+    assert params['t2/b2.params[W]'] == b2.params[1]
+    assert params['t2/b3.params[V]'] == b3.params[0]
+    assert params['t2/b3.params[W]'] == b3.params[1]
+    assert len(params) == 8
